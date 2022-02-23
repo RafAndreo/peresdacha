1ad255514d99382d6b6a50ece245814a5fba792a
RafAndreo <75218948+RafAndreo@users.noreply.github.com>
2022-02-24 02:49:03 +0300

    peresdacha

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Avatar from './components/avatar'
+
+ReactDOM.render(
+  <div>
+      <Avatar/>
+  </div>,
+  document.getElementById('root')
+);
+
+
